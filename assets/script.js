0b040c7ac9374f24ba2d4cbf6dfebc4bd79ecbd6
hanafi mohamed <mohamed-69120@outlook.fr>
2024-07-29 22:08:41 +0200

    Ajouter des écouteurs d'événements pour les flèches

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,20 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+    {
+        "image": "slide1.jpg",
+        "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image": "slide2.jpg",
+        "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image": "slide3.jpg",
+        "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image": "slide4.png",
+        "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+    }
 ];
 
 // Initialiser la diapositive actuelle
@@ -40,4 +40,21 @@ slides.forEach((slide, index) => {
 // Sélectionner tous les points
 const dots = document.querySelectorAll(".dot");
 
+
+
+
+
+// Ajouter des écouteurs d'événements pour les flèches
+arrow_left.addEventListener("click", () => {
+    console.log("j'ai cliqué sur la flèche de gauche");
+    currentSlide = (currentSlide > 0) ? currentSlide - 1 : slides.length - 1; // Mettre à jour la diapositive actuelle
+
+    updateSlide(currentSlide); // Mettre à jour l'affichage
+});
+
+arrow_right.addEventListener("click", () => {
+    console.log("j'ai cliqué sur la flèche de droite");
+    currentSlide = (currentSlide < slides.length - 1) ? currentSlide + 1 : 0; // Mettre à jour la diapositive actuelle
+    updateSlide(currentSlide); // Mettre à jour l'affichage
+});
 
